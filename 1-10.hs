c4bea677b860e3156911f2b1359a807ddf005be4
Johan Andersson <johan.peter.andersson@gmail.com>
2014-02-08 00:31:12 +0100

    Added 3-10

--- 1-10.hs
+++ 1-10.hs
@@ -1,5 +1,38 @@
+import Data.List
+
 one :: [a] -> a
 one = last
 
 two :: [a] -> a
 two = (!!1) . reverse
+
+elementAt ::  [a] -> Int -> a
+elementAt l k = l !! (k-1) 
+
+myLength :: [a] -> Int
+myLength = length
+
+myReverse :: [a] -> [a]
+myReverse = reverse
+
+isPalindrome :: Eq a => [a] -> Bool
+isPalindrome list = list == (myReverse list)
+
+data NestedList a = Elem a | List [NestedList a]
+
+flatten :: NestedList a -> [a]
+flatten (Elem x) = [x]
+flatten (List x) = concatMap flatten x
+
+flatten3 :: NestedList a -> [a]
+flatten3 (Elem x) = [x]
+flatten3 (List xs) = foldr1 (++) $ map flatten3 xs
+
+compress :: Eq a => [a] -> [a]
+compress = map head . group
+
+pack :: Eq a => [a] -> [[a]]
+pack = group
+
+encode :: Eq a => [a] ->[(Int,a)]
+encode = map (\t -> (length t,head t)) . pack
